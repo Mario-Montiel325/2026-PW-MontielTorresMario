8cbcdf8370ce5743ae1bfbcaf6b4b51efa647981
Mario-Montiel325 <mmontielt2400@alumno.ipn.mx>
2026-09-24 18:23:32 -0600

    Agrego JSON y app junto con talleres

--- Primer parcial/02Ejercicios/js/app.js
+++ Primer parcial/02Ejercicios/js/app.js
@@ -71,3 +71,54 @@ formArreglos.addEventListener('submit', (evento) =>{
 });
 
 pintarTabla();
+
+
+//Segunda parte
+
+const formObjeto = document.getElementById('form-objeto');
+const resultadoObjeto = document.getElementById('resultado-objeto');
+
+formObjeto.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    //construimos el objeto de talleres
+    const taller = {
+        nombre : document.getElementById('obj-nombre').value,
+        instructor : document.getElementById('obj-instructor').value,
+        cupo : Number(document.getElementById('obj-cupo')).value,
+        inscritos: Number(document.getElementById('obj-inscritos').value)
+
+    };
+
+    const operacion = document.getElementById('operacion-objeto').value;
+
+    let resultado
+
+    switch(operacion){
+        case 'keys':
+            resultado = JSON.stringify(Object.keys(taller));
+            break;
+        case 'values':
+            break;
+        case 'entries':
+            resultado = Object.entries(taller).map(([campo, valor]) => `$(campo):${valor}`).join('\n')
+            break;
+        case 'stringify':
+            break;
+        case 'roundtrip':
+            const textoJson = JSON.stringify(taller, null, 2);
+            const objetoDeVuelta = JSON.parse(textoJson);
+
+            resultado = [
+                '',
+                textoJson,
+                '',
+                `tipo: ${typeof objetoDeVuelta}`,
+                objetoDeVuelta.nombre
+                ].join('\n')
+            break;
+    }
+
+    resultadoObjeto.textContent = resultado;
+
+})
